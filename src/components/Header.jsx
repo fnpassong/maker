fa56cc960c06ca815b527c9d276bfd04bc714a26
fnpassong <139728483+fnpassong@users.noreply.github.com>
2024-07-14 19:53:18 -0500

    se movió elemento <div> (2), a main__images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,11 +12,11 @@ const Header = () => {
                 <div className="main__images">
                     <div className="main__img"></div>
                     <div className="main__right"></div>
-                </div>
-                <div className="main__text-area wrapper">
+                    <div className="main__text-area wrapper">
                     <h2 className="main__title">Get paid for the work you <span>love</span> to do.</h2>
                     <p className="main__paragraph">The 9-5 grind is so last century. We believe in living life on your own terms. Whether you’re looking to escape the rat race or set up a side hustle, we’ve got you covered.</p>
                     <img className='main__scroll' src={ scroll } alt="" />
+                </div>
                 </div>
             </div>
         </div>
