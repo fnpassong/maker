da08e8afa8fdee04d7554a23109d318b90643167
fnpassong <139728483+fnpassong@users.noreply.github.com>
2024-07-14 19:29:54 -0500

    se finalizo section-I para solo mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
+import logo from '../assets/logo.svg';
+import scroll from '../assets/icon-scroll.svg';
 
+import '../sass/components/header.scss'
 
 const Header = () => {
   return (
-    <section>
-        <h2>Hola</h2>
+    <section className='main'>
+        <div className="main__wrapper">
+            <div className="logo"><img src={ logo } alt="" /></div>
+            <div className="main__many-parts">
+                <div className="main__images">
+                    <div className="main__img"></div>
+                    <div className="main__right"></div>
+                </div>
+                <div className="main__text-area wrapper">
+                    <h2 className="main__title">Get paid for the work you <span>love</span> to do.</h2>
+                    <p className="main__paragraph">The 9-5 grind is so last century. We believe in living life on your own terms. Whether you’re looking to escape the rat race or set up a side hustle, we’ve got you covered.</p>
+                    <img className='main__scroll' src={ scroll } alt="" />
+                </div>
+            </div>
+        </div>
     </section>
   )
 }
